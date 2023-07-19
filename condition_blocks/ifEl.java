1169c03c44732d2e6802f04d9c5fbfaa2b963bf3
Melih AKMAN <melih-akman@outlook.com>
2023-07-19 18:22:28 +0300

    Create ifEl.java

---
+++ condition_blocks/ifEl.java
@@ -0,0 +1,14 @@
+package condition_blocks;
+
+public class ifEl {
+    public static void main(String[] args) {
+        int a = 5;
+        if (a == 5) {
+            System.out.println("a is equal to 5");
+        } else if (a == 6) {
+            System.out.println("a is equal to 6");
+        } else {
+            System.out.println("a is not equal to 5 or 6");
+        }
+    }
+}
