78f16ed1d615bde920a81c8e05f1b1e385817916
Melih AKMAN <melih-akman@outlook.com>
2023-07-21 19:12:07 +0300

    Create customerManager.java

---
+++ classes/classEx/customerManager.java
@@ -0,0 +1,14 @@
+package classes.classEx;
+
+public class customerManager {
+    public void add(String customerName) {
+        System.out.println("Customer added "+customerName);
+    }
+    public void remove() {
+        System.out.println("Customer removed");
+    } 
+    public void update() {
+        System.out.println("Customer updated");
+    }
+
+}
