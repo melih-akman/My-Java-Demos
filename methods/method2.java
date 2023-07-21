bc9c75ecf78c561367012aa293c17a113df052cf
Melih AKMAN <melih-akman@outlook.com>
2023-07-21 19:12:13 +0300

    Create method2.java

---
+++ methods/method2.java
@@ -0,0 +1,12 @@
+package methods;
+
+public class method2 {
+    public static void main(String[] args) {
+        System.out.println(minus(3, 5));
+    }
+
+    public static int minus(int a, int b) {
+        return a + b;
+    }
+
+}
