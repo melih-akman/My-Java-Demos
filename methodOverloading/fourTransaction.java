3d8083c79d74ecfa4c57295089375604c8e826d8
Melih AKMAN <melih-akman@outlook.com>
2023-07-22 15:53:19 +0300

    Create fourTransaction.java

---
+++ methodOverloading/fourTransaction.java
@@ -0,0 +1,11 @@
+package methodOverloading;
+
+public class fourTransaction {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    
+}
