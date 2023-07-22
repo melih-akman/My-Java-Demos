32f56c1e87787f4573deb0bf62f6d323520980d6
Melih AKMAN <melih-akman@outlook.com>
2023-07-22 16:28:46 +0300

    Create inheritances.java

---
+++ inheritance/inheritances.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class inheritances {
+    public static void main(String[] args) {
+        customer customerData = new customer();
+        employee employeeData = new employee();
+        employeeManager employeeManagerData = new employeeManager();
+        customerManager customerManagerData = new customerManager();
+        
+    }
+}
