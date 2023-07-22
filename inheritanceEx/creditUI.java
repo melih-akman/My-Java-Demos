e214b7d1699ce73aa684d551a9c647ccfaddc2f4
Melih AKMAN <melih-akman@outlook.com>
2023-07-22 16:28:51 +0300

    Create creditUI.java

---
+++ inheritanceEx/creditUI.java
@@ -0,0 +1,7 @@
+package inheritanceEx;
+
+public class creditUI {
+    public void creditCalculate(teacherCreditManager teacherCreditManagerData) {
+        teacherCreditManagerData.calculate();
+    }
+}
