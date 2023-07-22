4dfd46becacf40d1a0e2c3ca34b3d11a63b09429
Melih AKMAN <melih-akman@outlook.com>
2023-07-22 15:54:36 +0300

    Update main.java

--- methodOverloading/main.java
+++ methodOverloading/main.java
@@ -1,6 +1,6 @@
 package methodOverloading;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         fourTransaction fourTransaction = new fourTransaction();
         System.out.println(fourTransaction.add(2, 3));
